6f87c2ee4304aa3de6b21e4c050fc0c6da112ab1
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-20 21:41:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
       new_string += "  #{holiday.capitalize}: "
       i = 0 
       while i < items.length - 1 
-        new_string += items[i]","
+        new_string += "#{items[i]},"
       end
       new_string += items[items.length - 1]
       puts new_string
