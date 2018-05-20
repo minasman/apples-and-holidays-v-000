431c11393400b76d79c6696c0e12f9a0445701ec
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-20 21:58:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,6 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
       else
         temp.capitalize
       end
+      binding.pry 
       new_string += "  #{temp}: "
       i = 0 
       while i < items.length - 1 
